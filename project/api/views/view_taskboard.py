75e0182ac66210dabf7b3ceaf1ef06c088677ed9
Thonq <notho1002@gmail.com>
2025-01-03 00:52:02 +0700

    Initial commit

--- project/api/views/view_taskboard.py
+++ project/api/views/view_taskboard.py
@@ -1,13 +1,23 @@
-from rest_framework import viewsets, status
+from rest_framework import viewsets, status, filters
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
 from api.models import TaskBoard, Employee
 from api.serializers import TaskBoardSerializer
+
+class TaskBoardPagination(PageNumberPagination):
+    page_size = 10
+    page_size_query_param = 'page_size'
+    max_page_size = 100
 
 class TaskBoardViewSet(viewsets.ModelViewSet):
     queryset = TaskBoard.objects.all()
     serializer_class = TaskBoardSerializer
     permission_classes = [IsAuthenticated]
+    pagination_class = TaskBoardPagination
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]  
+    search_fields = ['title', 'description']
 
     def create(self, request, *args, **kwargs):
         title = request.data.get('title')
@@ -21,6 +31,20 @@ class TaskBoardViewSet(viewsets.ModelViewSet):
     
     def perform_create(self, serializer):
         serializer.save(created_by=self.request.user)
+    
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            search_query = self.request.query_params.get('search', None)
+            if search_query:
+                search_terms = search_query.strip('"')
+                for term in search_terms.split():
+                    page = [item for item in page if term.lower() in item.title.lower() or term.lower() in item.description.lower()]
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
 
     def update(self, request, *args, **kwargs):
         taskboard_id = kwargs.get('pk')
@@ -37,16 +61,3 @@ class TaskBoardViewSet(viewsets.ModelViewSet):
         instance = self.get_object()
         self.perform_destroy(instance)
         return Response({'message': f'TaskBoard {taskboard_id} has been deleted successfully'}, status=status.HTTP_200_OK)
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.get_queryset()
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            search_query = self.request.query_params.get('name', None)
-            if search_query:
-                search_terms = search_query.strip('"')
-                for term in search_terms.strip('"'):
-                    term = term.strip('"')
-                    page = [item for item in page if term.lower() in item.name.lower()]
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
--- project/project/settings.py
+++ project/project/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'drf_spectacular',
     'drf_spectacular_sidecar',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -82,6 +83,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend',],
 }
 
 SPECTACULAR_SETTINGS = {
