42bf64be873bcd8909a2140b1b4474725661f629
Thonq <notho1002@gmail.com>
2025-01-02 22:18:43 +0700

    Initial commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -9,8 +9,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-=584ud6d8abh4=w+!6*71!k)03zt@y1wqpw-ohgrb5dcr(h^_a')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
+
 
 ALLOWED_HOSTS = ['.onrender.com', 'localhost', '127.0.0.1']
 
