244997c3a26435db374f90d95caa70fc1bc32135
Thonq <notho1002@gmail.com>
2025-01-02 21:55:04 +0700

    Initial commit

--- project/project/settings.py
+++ project/project/settings.py
@@ -41,7 +41,6 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 ROOT_URLCONF = 'project.urls'
