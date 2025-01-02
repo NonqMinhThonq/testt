76f40360c7debeb8879d2e8088041a8716ddb3db
Thonq <notho1002@gmail.com>
2025-01-02 22:15:55 +0700

    Initial commit

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -1,6 +1,9 @@
 import os
-
+import sys
 from django.core.wsgi import get_wsgi_application
+
+print("WSGI file executed")
+print("sys.path:", sys.path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
 
